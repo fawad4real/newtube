5f66a649816cebc929432dc778fab08b5e0b7db3
Fawad Javed <82650202+fawad4real@users.noreply.github.com>
2025-02-18 00:27:20 +0500

    added upstsah redis and ratelimit and trpc configuration

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -2,6 +2,8 @@ import {
     defaultShouldDehydrateQuery,
     QueryClient,
   } from '@tanstack/react-query';
+  import superjson from 'superjson';
+  
 //   import superjson from 'superjson';
   export function makeQueryClient() {
     return new QueryClient({
@@ -16,7 +18,7 @@ import {
             query.state.status === 'pending',
         },
         hydrate: {
-          // deserializeData: superjson.deserialize,
+          deserializeData: superjson.deserialize,
         },
       },
     });
